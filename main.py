51615a7a99396cf0c078f7c451db0d3b6dab182e
aatreOo <pejayoo@gmail.com>
2025-09-07 20:03:42 +0200

    word count

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_text = get_book_text()
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
+
+if __name__ == "__main__":
+    main()
